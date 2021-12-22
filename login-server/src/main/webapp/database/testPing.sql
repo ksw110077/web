172f3b2f9401aceaca8dc34ceec7694fd62480dd
Administrator <Administrator@220.86.167.23>
2021-12-22 19:52:19 +0900

    SQL 파일 업데이트

---
+++ login-server/src/main/webapp/database/testPing.sql
@@ -0,0 +1,19 @@
+select * from users;
+
+-- create schema 데이터베이스명;
+-- create table
+-- insert
+
+--create schema loginServer;
+--use loginServer;
+--
+--create table users(
+--	code integer primary key auto_increment,
+--	id varchar(30) not null,
+--    pw varchar(30) not null,
+--    regDate datetime not null default current_timestamp
+--);
+--
+--insert users(id, pw) values('apple', '1234');
+--insert users(id, pw) values('banana','1234');
+--insert users(id, pw) values('melon','1234');
